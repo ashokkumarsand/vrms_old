53b6e7d5a9563b48b84ee6f380ed702af135c1f1
ashokkumarsand <sand.ashok1@gmail.com>
2013-11-12 17:45:54 +0530

    User login created

--- vrms/src/java/com/vrms/authentication/Logout.java
+++ vrms/src/java/com/vrms/authentication/Logout.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -30,8 +31,12 @@ public class Logout extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            request.getSession(false).removeAttribute(Constants.USERID);
-            response.sendRedirect(Constants.LOGIN_PAGE);
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        response.sendRedirect(Constants.LOGIN_PAGE);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
